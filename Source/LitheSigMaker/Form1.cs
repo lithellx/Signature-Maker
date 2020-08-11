5bee4f03fbe97fc70df03f2fd44de893ffb66a4e
lithé <45209073+lithellx@users.noreply.github.com>
2020-08-11 16:15:13 +0300

    Add files via upload

---
+++ Source/LitheSigMaker/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LitheSigMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*AOB*/
+            string AOB = " " + textBox1.Text;
+            /*AOB*/
+
+            /*Signature*/
+            string Signature = AOB.Replace(" ", @"\x");
+            textBox2.Text = Signature;
+            /*Signature*/
+
+            /* Mask*/
+            string MaskMaker = AOB.Replace(" ", "");
+            int m = MaskMaker.Length / 2;
+            for (string Mask = ""; Mask.Length < m; Mask+="x")
+            {
+                textBox3.Text = Mask + "x";
+            }
+            /* Mask*/
+
+        }
+    }
+}
